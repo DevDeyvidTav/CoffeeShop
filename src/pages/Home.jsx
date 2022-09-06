65f8bc2acba3f10b433eb788e2bb2363deebd6c5
Deyvid Tavares <devdeyvid2022@gmail.com>
2022-09-06 19:51:46 -0300

    melhorias no mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 
-
+import { NavLink } from "react-router-dom"
 
 export function Home() {
     return (
@@ -8,7 +8,7 @@ export function Home() {
                 <img className="w-screen md:w-3/5 h-72 md:h-screen shadow-xl shadow-zinc-500 " src="https://img.freepik.com/vetores-gratis/fundo-preto-e-branco-da-cafeteria_125540-817.jpg?w=2000" alt="" />
                 <div style={{backgroundColor:'#8C663C'}} className="w-screen md:w-2/5 h-screen flex items-center flex-col justify-center gap-20 ">
                     <p className="text-center text-white font-bold text-4xl mx-20 font-serif">Temos os melhores cafés e refeições da região. Tudo isso com um preço super acessível.</p>
-                    <button className='p-1 bg-white hover:bg-zinc-300 border-2 border-black font-serif rounded-md px-3 hover:duration-300 shadow-xl text-black font-bold text-2xl '>Conheça nossa história</button>
+                    <NavLink to="/history"><button className='p-1 bg-white hover:bg-zinc-300 border-2 border-black font-serif rounded-md px-3 hover:duration-300 shadow-xl text-black font-bold text-2xl '>Conheça nossa história</button></NavLink>
                 </div>
             </div>
         </div>
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -52,16 +52,16 @@ export function Menu() {
           </div>
         </SwiperSlide>
       </Swiper>
-      <div className="lg:hidden Menu-mobile| flex flex-col gap-20 mt-20 ">
-        <div className="bg-white flex items-center justify-center flex-col p-2 rounded-md">
+      <div className="lg:hidden Menu-mobile| text-2xl text-[#2d1b08] font-bold flex flex-col gap-20 mt-20 ">
+        <div className="shadow-2xl shadow-[#2d1b08] bg-white flex items-center justify-center flex-col p-2 rounded-md">
           <img className="w-96 h-80" src="https://st.depositphotos.com/3163695/4402/i/450/depositphotos_44023829-stock-photo-coffee.jpg" alt="" />
           <p className="mx-16">Café expresso</p>
         </div>
-        <div className="bg-white flex items-center justify-center flex-col p-2 rounded-md">
+        <div className="shadow-2xl shadow-[#2d1b08] bg-white flex items-center justify-center flex-col p-2 rounded-md">
           <img className="w-96 h-80" src="https://st.depositphotos.com/2363887/2571/i/450/depositphotos_25717699-stock-photo-cappuccino-mug-close-up-with.jpg" alt="" />
           <p className="mx-16">café com leite</p>
         </div>
-        <div className="bg-white flex items-center justify-center flex-col p-2 rounded-md">
+        <div className=" shadow-2xl shadow-[#2d1b08] bg-white flex items-center justify-center flex-col p-2 rounded-md">
           <img className="w-96 h-80" src="https://static5.depositphotos.com/1013073/406/i/450/depositphotos_4069405-stock-photo-latte-macchiato-with-cocoa-and.jpg" alt="" />
           <p className="mx-16">Late macciato</p>
         </div>
